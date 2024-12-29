56b183f18f7d06d5297173f82d5c2918f8530c33
Caris Williams <cwill1013@gmail.com>
2024-12-28 19:44:03 -0500

    Added method to calculate the area of a circle

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Module1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            if (double.TryParse(Console.ReadLine(), out double radius) && radius >= 0) 
+            {
+                double area = CalculateCircleArea(radius);
+
+                Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
+            }
+        }
+
+        static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
